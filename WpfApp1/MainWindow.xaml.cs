dbcfce1028f7d5e0076afddf1c3173573a09df41
Alexander Levchenko <alexandrl@risa.com>
2018-07-16 18:03:53 +0300

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //public string Label { get; set; } = "Some text";
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Line myLine = new Line();
+            //myLine.Stroke = System.Windows.Media.Brushes.LightSteelBlue;
+            //myLine.X1 = 1;
+            //myLine.X2 = 50;
+            //myLine.Y1 = 1;
+            //myLine.Y2 = 50;
+            //myLine.HorizontalAlignment = HorizontalAlignment.Left;
+            //myLine.VerticalAlignment = VerticalAlignment.Center;
+            //myLine.StrokeThickness = 2;
+            //member_graph mb = new member_graph();
+            //mygrid.Children.Add(mb);
+        }
+    }
+}
---
+++ WpfApp1/MemberDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class MemberDTO
+    {
+        public string Label { get; set; } = "M25";
+        public string INode { get; set; } = "INode";
+        public string JNode { get; set; } = "JNode";
+
+        public string IRealese{ get; set; } = "IRealese";
+        public string JRealese { get; set; } = "JRealese";
+
+        public double IOffset { get; set; } = 100;
+        public double JOffset { get; set; } = 100;
+
+        public double length { get; set; } = 700;
+
+
+        public double lengthloffset { get => (IOffset / length) * 100; }
+        
+        public double lengthroffset { get => 100 - (JOffset / length) * 100; }
+        public bool IsINode { get => IRealese != null ? true : false; }
+        public bool IsJNode { get => JRealese != null ? true : false; }
+
+    }
+}
