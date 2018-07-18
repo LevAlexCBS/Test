237965461a2bff5faf4029233ecf83eff798ca2e
Alexander Levchenko <alexandrl@risa.com>
2018-07-18 14:52:07 +0300

    dfg

--- WpfApp1/MemberDTO.cs
+++ WpfApp1/MemberDTO.cs
@@ -8,7 +8,7 @@ namespace WpfApp1
 {
     public class MemberDTO
     {
-        public string Label { get; set; } = "M25";
+        public string MemberLabel { get; set; } = "M25";
         public string INode { get; set; } = "INode";
         public string JNode { get; set; } = "JNode";
 
@@ -18,12 +18,19 @@ namespace WpfApp1
         public double IOffset { get; set; } = 100;
         public double JOffset { get; set; } = 100;
 
-        public double length { get; set; } = 700;
+        public double length { get; set; } = 1000;
 
 
         public double lengthloffset { get => (IOffset / length) * 100; }
         
         public double lengthroffset { get => 100 - (JOffset / length) * 100; }
+
+        public double IOffsetRelative { get; set; } = 350;
+        public double JOffsetRelative { get; set; } = 750;
+        public bool IsICircleVisible { get; set; }
+        public bool IsJCircleVisible { get; set; }
+        public bool IsIEllipseVisible { get; set; }
+        public bool IsJEllipseVisible { get; set; }
         public bool IsINode { get => IRealese != null ? true : false; }
         public bool IsJNode { get => JRealese != null ? true : false; }
 
