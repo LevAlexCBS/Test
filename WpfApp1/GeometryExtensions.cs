ef68dcdee3be079d3e231af9dfbe067724457574
Alexander Levchenko <alexandrl@risa.com>
2018-07-20 14:18:43 +0300

    hfgdh

---
+++ WpfApp1/GeometryExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    public static class GeometryExtensions
+    {
+        public static void DrawGeometry(this StreamGeometryContext ctx, Geometry geo, bool isFilled)
+        {
+            var pathGeometry = geo as PathGeometry ?? PathGeometry.CreateFromGeometry(geo);
+            foreach (var figure in pathGeometry.Figures)
+            {
+                figure.IsFilled = isFilled;
+                ctx.DrawFigure(figure);
+            }
+        }
+
+        public static void DrawFigure(this StreamGeometryContext ctx, PathFigure figure)
+        {
+            ctx.BeginFigure(figure.StartPoint, figure.IsFilled, figure.IsClosed);
+            foreach (var segment in figure.Segments)
+            {
+                if (segment is LineSegment lineSegment)
+                {
+                    ctx.LineTo(lineSegment.Point, lineSegment.IsStroked, lineSegment.IsSmoothJoin);
+                }
+
+                if (segment is BezierSegment bezierSegment)
+                {
+                    ctx.BezierTo(bezierSegment.Point1, bezierSegment.Point2, bezierSegment.Point3, bezierSegment.IsStroked, bezierSegment.IsSmoothJoin);
+                }
+
+                if (segment is QuadraticBezierSegment quadraticSegment)
+                {
+                    ctx.QuadraticBezierTo(quadraticSegment.Point1, quadraticSegment.Point2, quadraticSegment.IsStroked, quadraticSegment.IsSmoothJoin);
+                }
+
+                if (segment is PolyLineSegment polyLineSegment)
+                {
+                    ctx.PolyLineTo(polyLineSegment.Points, polyLineSegment.IsStroked, polyLineSegment.IsSmoothJoin);
+                }
+
+                if (segment is PolyBezierSegment polyBezierSegment)
+                {
+                    ctx.PolyBezierTo(polyBezierSegment.Points, polyBezierSegment.IsStroked, polyBezierSegment.IsSmoothJoin);
+                }
+
+                if (segment is PolyQuadraticBezierSegment polyQuadraticSegment)
+                {
+                    ctx.PolyQuadraticBezierTo(polyQuadraticSegment.Points, polyQuadraticSegment.IsStroked, polyQuadraticSegment.IsSmoothJoin);
+                }
+
+                if (segment is ArcSegment arcSegment)
+                {
+                    ctx.ArcTo(arcSegment.Point, arcSegment.Size, arcSegment.RotationAngle, arcSegment.IsLargeArc, arcSegment.SweepDirection, arcSegment.IsStroked, arcSegment.IsSmoothJoin);
+                }
+            }
+        }
+    }
+}
--- WpfApp1/UserControl3.xaml.cs
+++ WpfApp1/UserControl3.xaml.cs
@@ -23,38 +23,36 @@ namespace WpfApp1
         public UserControl3()
         {
             InitializeComponent();
-            UIElement[] _ploads = new UIElement[6];
-            for (int i = 0; i < 3; i++)
-            {
-                var arrow = new Polygon()
-                {
-                    Fill = Brushes.Blue,
-                    Points = new PointCollection()
-                    {
-                        new Point(0, -25),
-                        new Point(15, 4),
-                        new Point(30, -25)
-                    }
-                };
-                var line = new Line()
-                {
-                    Stroke = Brushes.Blue,
-                    X1 = 15,
-                    Y1 = 0,
-                    X2 = 15,
-                    Y2 = -100
-                };
-                var text = new TextBlock(){Text = "-15k"};
-                Canvas.SetLeft(arrow, i * 100);
-                Canvas.SetLeft(line, i * 100);
-                canvas.Children.Add(arrow);
-                canvas.Children.Add(line);
-                Canvas.SetLeft(text, (i * 100) - 20);
-                Canvas.SetBottom(text, 100);
-                canvas.Children.Add(text);
-            }
-            var trg = new Triangle() { Stroke = Brushes.Black };
-            canvas.Children.Add(trg);
+            //UIElement[] _ploads = new UIElement[6];
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    var arrow = new Polygon()
+            //    {
+            //        Fill = Brushes.Blue,
+            //        Points = new PointCollection()
+            //        {
+            //            new Point(0, -25),
+            //            new Point(15, 4),
+            //            new Point(30, -25)
+            //        }
+            //    };
+            //    var line = new Line()
+            //    {
+            //        Stroke = Brushes.Blue,
+            //        X1 = 15,
+            //        Y1 = 0,
+            //        X2 = 15,
+            //        Y2 = -100
+            //    };
+            //    var text = new TextBlock(){Text = "-15k"};
+            //    Canvas.SetLeft(arrow, i * 100);
+            //    Canvas.SetLeft(line, i * 100);
+            //    canvas.Children.Add(arrow);
+            //    canvas.Children.Add(line);
+            //    Canvas.SetLeft(text, (i * 100) - 20);
+            //    Canvas.SetBottom(text, 100);
+            //    canvas.Children.Add(text);
+            //}
 
         }
     }
